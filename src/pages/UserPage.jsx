783181f303962364db0d62fb7b5b031bcfe4ee4b
semarslan <sema-arslan@bil.omu.edu.tr>
2020-11-03 19:44:18 +0300

    getByUser api ve hata mesajı eklendi.

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ProfileCard from "../components/ProfileCard";
-
+import {getUser} from '../api/apiCalls';
 
 const UserPage = (props) => {
+    const [user, setUser] = useState();
+
+    const loadUser = async ()  => {
+        await getUser(props.match.params.username);
+    }
     return (
         <div className="container">
             <ProfileCard />
