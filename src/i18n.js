05b5de7fba3793f4b278345b58a2880465bf8e92
semarslan <sema-arslan@bil.omu.edu.tr>
2020-11-04 23:59:37 +0300

    Uesr not found mesajı eklendi icon eklendi

--- src/i18n.js
+++ src/i18n.js
@@ -20,7 +20,8 @@ i18n.use(initReactI18next).init({
                 Users: "Users",
                 Next: "Next >",
                 Previous: "< Previous",
-                'Load Failure': "Load Failure"
+                'Load Failure': "Load Failure",
+                'User not found' : "User not found"
             }
         },
         tr: {
@@ -39,7 +40,8 @@ i18n.use(initReactI18next).init({
                 Users: "Kullanıcılar",
                 Next: "Sonraki >",
                 Previous: "< Önceki",
-                'Load Failure' : "Liste Alınamadı"
+                'Load Failure' : "Liste Alınamadı",
+                'User not found' : "Kullanıcı bulunamadı"
             }
         }
     },
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -2,26 +2,42 @@ import React, {useEffect, useState} from 'react';
 import ProfileCard from "../components/ProfileCard";
 import {getUser} from '../api/apiCalls';
 import {useParams} from 'react-router-dom';
+import {useTranslation} from "react-i18next";
 
 const UserPage = () => {
 
     const [user, setUser] = useState();
-
+    const [notFound, setNotFound] = useState(false);
     const {username} = useParams();
+    const {t} = useTranslation();
+    useEffect(() => {
+        setNotFound(false);
+    }, [user])
 
     useEffect(() => {
         const loadUser = async () => {
             try {
                 const response = await getUser(username);
-                setUser(response.data)
-
-            } catch (error){
-
+                setUser(response.data);
+            } catch (error) {
+                setNotFound(true);
             }
         };
         loadUser();
     }, [username])
 
+    if (notFound) {
+        return (
+            <div className="container">
+                <div className="alert alert-danger text-center" role="alert">
+                    <div>
+                        <span className="material-icons" style={{fontSize: '48px'}}>error</span>
+                    </div>
+                    {t('User not found')}
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="container">
             <ProfileCard/>
