f39b550b2898dc6a36c812421e8d2d48bdbbae46
semarslan <sema-arslan@bil.omu.edu.tr>
2020-11-19 20:38:55 +0300

    hoax zamanları için paket yüklendi, dil desteği ile birlikte eklendi.

--- src/components/HoaxView.jsx
+++ src/components/HoaxView.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import ProfilePicture from "./ProfilePicture";
 import {Link} from "react-router-dom";
+import {format} from "timeago.js";
+import {useTranslation} from "react-i18next";
 
 const HoaxView = (props) => {
     const {hoax} = props;
-    const {user, content} = hoax;
+    const {user, content, date} = hoax;
     const {username, displayName, image} = user;
+
+    const {i18n} = useTranslation();
+
+    const formatted = format(date, i18n.language);
     return (
         <div className="card p-1 m-2">
             <div className="d-flex">
                 <ProfilePicture image={image} width={32} height={32} className="rounded-circle m-1"/>
                 <div className="flex-fill m-auto pl-2">
                     <Link to={`/user/${username}`} className="text-dark ">
-                        <h6>{displayName}@{username}</h6>
+                        <h6 className="d-inline">{displayName}@{username}</h6>
                     </Link>
+                    <span> - </span>
+                    {formatted}
                 </div>
             </div>
             <div className="pl-5">
-                {hoax.content}
+                {content}
             </div>
         </div>
     );
